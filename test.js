5faf5cf7bdaf324e2d85fdd384839a5af08b3009
Younes <bami.younes@gmail.com>
2020-04-23 05:24:43 +0000

    modify test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 
 describe('String#split', function(){
-  it('should have had had  returned an array ', function(){
+  it('Yes should have had had  returned an array ', function(){
     assert(Array.isArray('a,b,c'.split(',')));
   });
 });
